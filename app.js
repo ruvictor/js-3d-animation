0bf89d4bb44c0ace2201692c06b00daa744722ee
Victor <rusuvictor30@gmail.com>
2020-11-23 20:38:19 -0500

    Final 1

--- app.js
+++ app.js
@@ -1,41 +1,33 @@
 // Movement animation to happen
-const card = document.querySelector('.card');
+const iPhone = document.querySelector('.iPhone');
 const container = document.querySelector('.container');
 
 //items
-const title = document.querySelector('.title');
-const sneaker = document.querySelector('.sneaker img');
-const purchase = document.querySelector('.purchase button');
-const description = document.querySelector('.info h3');
-const sizes = document.querySelector('.sizes');
+const dunkinLogo = document.querySelector('.dunkinLogo');
+const donuts = document.querySelector('.donuts');
+const content = document.querySelector('.content h3');
 
 // Moving Animation Event
 container.addEventListener('mousemove', (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+    iPhone.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 
 
-// animate in
+// // animate in
 container.addEventListener('mouseenter', e => {
-    card.style.transition = 'none';
-    // pupout
-    title.style.transform = 'translateZ(150px)';
-    sneaker.style.transform = 'translateZ(200px) rotateZ(-45deg)';
-    description.style.transform = 'translateZ(125px)';
-    sizes.style.transform = 'translateZ(100px)';
-    purchase.style.transform = 'translateZ(75px)';
-})
+    iPhone.style.transition = 'none';
+    dunkinLogo.style.transform = 'translateZ(150px) rotateZ(-45deg)';
+    donuts.style.transform = 'translateZ(150px)';
+    content.style.transform = 'translateZ(140px)';
+});
 
-//animate out
+// //animate out
 container.addEventListener('mouseleave', e => {
-    card.style.transition = "all 0.5s ease";
-    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-    // pupback
-    title.style.transform = 'translateX(0px)';
-    sneaker.style.transform = 'translateZ(0px) rotateZ(0deg)';
-    description.style.transform = 'translateZ(0px)';
-    sizes.style.transform = 'translateZ(0px)';
-    purchase.style.transform = 'translateZ(0px)';
+    iPhone.style.transition = "all 0.5s ease";
+    iPhone.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    dunkinLogo.style.transform = 'translateZ(0px) rotateZ(0deg)';
+    donuts.style.transform = 'translateZ(0px)';
+    content.style.transform = 'translateZ(0px)';
 });
